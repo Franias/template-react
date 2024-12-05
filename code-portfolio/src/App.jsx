5c5e57a26b3ef9b186c4af2318bbfa80ec724025
Francielli Pinheiro Dias <francielli_dias@terceiros.sicredi.com.br>
2024-12-05 16:40:40 -0300

    conclui sobre e projetos

--- code-portfolio/src/App.jsx
+++ code-portfolio/src/App.jsx
@@ -1,5 +1,13 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
-
+import './App.css';
+import profile from './assets/profile.jpg';
+import cypress from './assets/cypress.jpg';
+import selenium from './assets/selenium.png';
+/**
+ * Navbar component renders a navigation bar with a logo, navigation links, 
+ * and icon images. The links include 'Sobre', 'Currículo', and 'Projetos'.
+ */
 function Navbar() {
   return (
     <nav className="navbar">
@@ -7,14 +15,20 @@ function Navbar() {
         <img src="https://dashboard.codeparrot.ai/api/assets/Z1G3V0mqC8VE7xqg" alt="Logo" width="87" height="60" />
       </div>
       <ul className="navbar-links">
-        <li><a href="#">Sobre</a></li>
+        <li><a href="#sobreMim">Sobre</a></li>
         <li><a href="#">Currículo</a></li>
-        <li><a href="#">Projetos</a></li>
+        <li><a href="#meusProjetos">Projetos</a></li>
       </ul>
       <div className="navbar-icons">
-        <img src="https://dashboard.codeparrot.ai/api/assets/Z1G3V0mqC8VE7xqh" alt="Icon 1" width="24" height="24" />
-        <img src="https://dashboard.codeparrot.ai/api/assets/Z1G3V0mqC8VE7xqi" alt="Icon 2" width="24" height="24" />
-        <img src="https://dashboard.codeparrot.ai/api/assets/Z1G3V0mqC8VE7xqj" alt="Icon 3" width="24" height="24" />
+        <a href="https://github.com/Franias">
+          <img src="https://dashboard.codeparrot.ai/api/assets/Z1G3V0mqC8VE7xqh" alt="Icon 1" width="24" height="24" />
+        </a>
+        <a href="https://www.linkedin.com/in/francielli-pinheiro-dias-32b516187/">
+          <img src="https://dashboard.codeparrot.ai/api/assets/Z1G3V0mqC8VE7xqi" alt="Icon 2" width="24" height="24" />
+        </a>
+        <a href="mailto:francielli.dias@db.tec.br">
+          <img src="https://dashboard.codeparrot.ai/api/assets/Z1G3V0mqC8VE7xqj" alt="Icon 3" width="24" height="24" />
+        </a>
       </div>
     </nav>
   );
@@ -25,14 +39,31 @@ const ProfileCard = () => {
     <div style={styles.cardContainer}>
       <div style={styles.textContainer}>
         <h2 style={styles.greeting}>Olá, meu nome é</h2>
-        <h1 style={styles.name}>Ana Maria</h1>
-        <p style={styles.description}>Sou trainee de desenvolvimento de software na DBServer.</p>
+        <h1 style={styles.name}>Francielli Dias</h1>
+        <p style={styles.description}>Sou analista de qualidade de software e automatizo testes na DBServer.</p>
       </div>
       <img 
-        src="https://dashboard.codeparrot.ai/api/assets/Z1G3WEmqC8VE7xqk" 
+        src={profile}
         alt="Profile" 
         style={styles.image}
       />
+    </div>
+  );
+};
+
+const SobreMimCard = () => {
+  return (
+    <div style={styles.cardContainer}>
+      <div style={styles.textContainer}>
+        <h2 id="sobreMim">Sobre mim</h2>
+        <p style={styles.descriptionSobre}>
+        Desenvolvo testes automatizados utilizando Java, Javascript e SQL, com as ferramentas de automação como Cypress, Selenium, RestAssured, Postman, Insomnia, TestNG e JUnit.  
+Trabalho com garantia de qualidade há 5 anos e participo de projetos durante todo o ciclo de desenvolvimento do software, assegurando a execução de testes de código junto aos desenvolvedores e promovendo um melhor refinamento e escrita das histórias de usuário no projeto.  
+Elaboro critérios de aceitação junto aos Analistas de Negócios e PO, além de monitorar, evidenciar e documentar qualquer alteração no projeto.  
+Na documentação e formalização de testes utilizo BDD, casos de teste, cenários de teste e scripts de teste com seus passo a passo.  
+Crio estratégias de qualidade para as mudanças no código, identificando riscos potenciais e as áreas possivelmente afetadas.  
+Tenho conhecimento em desenvolvimento de software com metodologias ágeis e em cascata.</p>
+      </div>
     </div>
   );
 };
@@ -72,6 +103,12 @@ const styles = {
     fontFamily: 'TTSupermolot-Regular',
     lineHeight: '44.28px',
   },
+  descriptionSobre: {
+    fontSize: '20px',
+    color: '#292929',
+    fontFamily:['TTSupermolot-Bold', 'Arial', 'sans-serif'],
+    lineHeight: '44.28px',
+  },
   image: {
     width: '292px',
     height: '292px',
@@ -80,11 +117,11 @@ const styles = {
   },
 };
 
-const ProjectCard = ({ imageId, title, tags, description }) => {
+const ProjectCard = ({ imageId, title, tags, description, url }) => {
   return (
     <div className="project-card">
       <img 
-        src={`https://placeholder.pics/svg?${imageId}`} 
+        src={imageId} 
         alt={title} 
         className="project-image"
       />
@@ -92,7 +129,9 @@ const ProjectCard = ({ imageId, title, tags, description }) => {
         <h2 className="project-title">{title}</h2>
         <p className="project-tags">{tags}</p>
         <p className="project-description">{description}</p>
-        <button className="project-button">Acessar</button>
+        <a href={url}>
+          <button  className="project-button">Acessar</button>
+        </a>
       </div>
     </div>
   );
@@ -103,27 +142,35 @@ function App() {
     <div className="App">
       <Navbar />
       <ProfileCard />
+      
+      <SobreMimCard />
       <div className="projects-section">
-        <h2>Meus Projetos</h2>
+        <h2 id="meusProjetos" >Meus Projetos</h2>
         <div className="projects-list">
           <ProjectCard 
-            imageId="1021:406" 
-            title="Calculadora" 
-            tags="HTML, CSS, NODE" 
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed diam urna, euismod at porttitor in, dignissim eu mauris. In sem nibh, viverra eu risus sed, accumsan luctus dui. Fusce ex neque, maximus quis dui et, rutrum sollicitudin sem."
+            imageId={cypress} 
+            title="Cypress Bugbank" 
+            tags="Javascript, NODE" 
+            description="Automação de testes end-to-end (E2E) usando o Cypress com NODE e Javascript.
+            O objetivo deste projeto é criar testes automatizados que simulem o comportamento do usuário em um navegador web, verificando se é possível realizar uma transação."
+            url= "https://github.com/Franias/cypress-bugbank"
+
           />
           <ProjectCard 
-            imageId="1021:407" 
-            title="Blog pessoal" 
-            tags="HTML, CSS, NODE" 
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed diam urna, euismod at porttitor in, dignissim eu mauris. In sem nibh, viverra eu risus sed, accumsan luctus dui. Fusce ex neque, maximus quis dui et, rutrum sollicitudin sem."
+            imageId={selenium} 
+            title="Selenium Bugbank" 
+            tags="Java, Maven" 
+            description="Automação de testes end-to-end (E2E) usando o Selenium com Maven e Java 19.
+            O objetivo deste projeto é criar testes automatizados que simulem o comportamento do usuário em um navegador web, verificando se é possível realizar uma transação."
+            url="https://github.com/Franias/selenium-bugbank"
+
           />
-          <ProjectCard 
+          {/* <ProjectCard 
             imageId="1021:408" 
             title="Mobile" 
             tags="Android studio, Kotlin" 
             description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed diam urna, euismod at porttitor in, dignissim eu mauris. In sem nibh, viverra eu risus sed, accumsan luctus dui. Fusce ex neque, maximus quis dui et, rutrum sollicitudin sem."
-          />
+          /> */}
         </div>
       </div>
     </div>
