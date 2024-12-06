38589ccabcaf0c2af1348b7a046110effde662ba
Francielli Pinheiro Dias <francielli_dias@terceiros.sicredi.com.br>
2024-12-06 16:04:09 -0300

    refatora layout de habilidades e sobremim

--- code-portfolio/src/App.jsx
+++ code-portfolio/src/App.jsx
@@ -57,12 +57,15 @@ const SobreMimCard = () => {
       <div style={styles.textContainer}>
         <h2 id="sobreMim">Sobre mim</h2>
         <p style={styles.descriptionSobre}>
-        Desenvolvo testes automatizados utilizando Java, Javascript e SQL, com as ferramentas de automação como Cypress, Selenium, RestAssured, Postman, Insomnia, TestNG e JUnit.  
-Trabalho com garantia de qualidade há 5 anos e participo de projetos durante todo o ciclo de desenvolvimento do software, assegurando a execução de testes de código junto aos desenvolvedores e promovendo um melhor refinamento e escrita das histórias de usuário no projeto.  
-Elaboro critérios de aceitação junto aos Analistas de Negócios e PO, além de monitorar, evidenciar e documentar qualquer alteração no projeto.  
+          Desenvolvo testes automatizados utilizando Java, Javascript e SQL, com as ferramentas de automação para front-end, back-end e banco de dados.  
+Trabalho com garantia de qualidade há 5 anos e participo de projetos durante todo o ciclo de desenvolvimento do software, assegurando a execução de testes de código junto aos desenvolvedores e promovendo um melhor refinamento e escrita das histórias de usuário no projeto.
+        </p>
+        <p style={styles.descriptionSobre}>
+        Elaboro critérios de aceitação junto aos Analistas de Negócios e PO, além de monitorar, evidenciar e documentar qualquer alteração no projeto.  
 Na documentação e formalização de testes utilizo BDD, casos de teste, cenários de teste e scripts de teste com seus passo a passo.  
 Crio estratégias de qualidade para as mudanças no código, identificando riscos potenciais e as áreas possivelmente afetadas.  
-Tenho conhecimento em desenvolvimento de software com metodologias ágeis e em cascata.</p>
+Tenho conhecimento em desenvolvimento de software com metodologias ágeis e em cascata.
+        </p>
       </div>
     </div>
   );
@@ -108,13 +111,15 @@ const styles = {
     color: '#292929',
     fontFamily:['TTSupermolot-Bold', 'Arial', 'sans-serif'],
     lineHeight: '44.28px',
+    textIndent: '50px',
   },
   image: {
     width: '292px',
     height: '292px',
     borderRadius: '8px',
     objectFit: 'cover',
-  },
+  }, 
+  
 };
 
 const ProjectCard = ({ imageId, title, tags, description, url }) => {
@@ -137,6 +142,152 @@ const ProjectCard = ({ imageId, title, tags, description, url }) => {
   );
 };
 
+const skillsTest = [
+  { name: 'Selenium', level: 4 },
+  { name: 'Cypress', level: 4 },
+  { name: 'Postman', level: 4 },
+  { name: 'Robot', level: 1 },
+  { name: 'Rest Assured', level: 3 }
+];
+
+const skillsLangague = [
+  { name: 'Java', level: 5 },
+  { name: 'Javascript', level: 4 },
+  { name: 'Git', level: 4 },
+  { name: 'React', level: 2 },
+  { name: 'Node', level: 3 }
+];
+
+const SkillRating = ({ level, maxLevel = 5 }) => {
+  const filledCircle = (
+    <svg width="24" height="24">
+      <circle cx="12" cy="12" r="10" fill="#40E0D0" />
+    </svg>
+  );
+
+  const outlinedCircle = (
+    <svg width="24" height="24">
+      <circle cx="12" cy="12" r="10" fill="none" stroke="#40E0D0" strokeWidth="2" />
+    </svg>
+  );
+  
+  const containerStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    gap: '4px',
+  };
+
+  const circleStyle = {
+    width: '24px',
+    height: '24px',
+  };
+  
+
+  return (
+    <div style={containerStyle}>
+      {[...Array(maxLevel)].map((_, index) => (
+        <div key={index}>
+          {index < level ? filledCircle : outlinedCircle}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+
+
+const SkillTestList = () => {
+  const containerStyle = {
+    fontFamily: 'Roboto Flex, sans-serif',
+    fontWeight: 300,
+    fontSize: '16px',
+    lineHeight: '18.75px',
+    color: '#000000',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    gap: '6px',
+    paddingLeft: '10px'
+  };
+
+  const skillStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+	  marginBottom: '8px',
+	  width: '100%',
+    paddingLeft: '10px'
+  };
+  
+
+  return (
+    <div style={containerStyle}>
+      {skillsTest.map((skill, index) => (
+        <div key={index} style={skillStyle}>
+          <span style={{ flex: 1 }}>{skill.name}</span>
+          <SkillRating level={skill.level} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const SkillLanguageList = () => {
+  const containerStyle = {
+    fontFamily: 'Roboto Flex, sans-serif',
+    fontWeight: 300,
+    fontSize: '16px',
+    lineHeight: '18.75px',
+    color: '#000000',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    gap: '6px',
+    paddingLeft: '10px'
+  };
+
+  const skillStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+	  marginBottom: '8px',
+	  width: '100%',
+    paddingLeft: '10px'
+  };
+  
+
+  return (
+    <div style={containerStyle}>
+      {skillsLangague.map((skill, index) => (
+        <div key={index} style={skillStyle}>
+          <span style={{ flex: 1 }}>{skill.name}</span>
+          <SkillRating level={skill.level} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const SkillComponent = () => {
+  const titleStyle = {
+    fontFamily: 'Roboto Flex, sans-serif',
+    fontWeight: 700,
+    fontSize: '24px',
+    lineHeight: '28px',
+    color: '#000000',
+    marginBottom: '16px'
+  };
+
+  return (
+    <div className="projects-section" style={styles.cardContainer}>
+      <h1 style={titleStyle}>Habilidades</h1>
+      <SkillTestList />
+      <SkillLanguageList />
+    </div>
+  );
+};
+
+
 function App() {
   return (
     <div className="App">
@@ -144,7 +295,9 @@ function App() {
       <ProfileCard />
       
       <SobreMimCard />
-      <div className="projects-section">
+
+      <SkillComponent />
+      <div className="projects-section" style={styles.cardContainer}>
         <h2 id="meusProjetos" >Meus Projetos</h2>
         <div className="projects-list">
           <ProjectCard 
